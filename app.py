e85a7f31b17621a40acc51ce59d47c0297acce89
sarahsbll <cynicoder@gmail.com>
2022-05-17 16:01:24 +0100

    Added initial webhook route

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello Again!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("Data received from Webhook is: ", request.json)
+        return "Webhook received!"
+
 

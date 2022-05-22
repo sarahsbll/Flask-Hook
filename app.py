90d7f2828ab1dfe8f9522e9ccf909c591f10ab12
sarahsbll <cynicoder@gmail.com>
2022-05-22 15:13:16 +0100

    debugging json file 15

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, request
+import dropbox
+
+#testdropbox
+# using an access token
+dbx = dropbox.Dropbox('sl.BIFVS2Po_OUNDI8q_DVraj12gqyKm4DrwwCenpc1Xy3Aq1hvzYwu5nBp8cevJ1Gt3nmctUc2J7J_kbQ0vrUU91G04dDVDptP81zr4OxSqUZgeTP2ZANOD68GAvOFQ8N0mCLlfGX2Wcb_')
+
+str_1 = "Join our freelance network"
+
+str_1_encoded = bytes(str_1,'UTF-8')
+
+dbx.files_upload(str_1_encoded, '/Python/test.txt')
+
+
+
+
 
 app = Flask(__name__)
-
-#testgithub
 @app.route("/")
 def index():
 	return "Hello World!"
@@ -10,7 +23,4 @@ def index():
 @app.route('/webhook', methods=['POST'])
 def webhook():
 	if request.method == 'POST':
-		with open("test.txt", "w") as outfile:
-			outfile.write("test")
-			outfile.close()
 		return "Webhook received!"

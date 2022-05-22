dca80ff1d4bd1b160ecd1b15384ad334d744f94f
sarahsbll <cynicoder@gmail.com>
2022-05-22 10:20:33 +0100

    debugging json file 4

--- app.py
+++ app.py
@@ -11,4 +11,10 @@ def index():
 def webhook():
     if request.method == 'POST':
         print("Data received from Webhook is: ", request.json)
+		# Serializing json 
+        json_object = json.dumps(request.get_json(), indent = 4)
+        # Writing to sample.json 
+        with open("webhooked.json", "a") as outfile:
+            outfile.write(json_object)
+            outfile.close()
         return "Webhook received!"

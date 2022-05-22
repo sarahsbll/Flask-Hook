cbc69ad904a915ef2fb4563df0fcd33869c7d144
sarahsbll <cynicoder@gmail.com>
2022-05-22 16:54:23 +0100

    debugging json file 20

--- app.py
+++ app.py
@@ -1,9 +1,52 @@
 from flask import Flask, request
 import dropbox
+import json
 
-#testdropbox
 # using an access token
-dbx = dropbox.Dropbox('sl.BIHXQMOID8C4j__clI2sqjyTAMYuAQZcHQKHdpZ3t41eHqsbQn4bjE2tPq68X3f7K0fgL-vaLx6iCqJNqvudHl9S3W6C3BMRujZAPhuBs6keEmtujAGFuSRPPPCIrH3_O2E8OEwLXRzK')
+#dbx = dropbox.Dropbox('sl.BIHXQMOID8C4j__clI2sqjyTAMYuAQZcHQKHdpZ3t41eHqsbQn4bjE2tPq68X3f7K0fgL-vaLx6iCqJNqvudHl9S3W6C3BMRujZAPhuBs6keEmtujAGFuSRPPPCIrH3_O2E8OEwLXRzK')
+
+
+
+#diction = {}
+
+li = []
+
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+        return "Hello World!"
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	if request.method == 'POST':
+		print("Data received from Webhook is: ", request.json)
+		li.append(request.get_json())
+		print(li)
+
+		#diction.update(request.get_json())
+		#print(diction)
+		#with open("test2.json", "a") as outfile:
+			#json.dump(diction, outfile)
+		#json_object = json.dumps(request.get_json(), indent = 4)
+		#with open("test.txt", "w") as outfile:            
+                        #outfile.write("test")
+                        #outfile.close()
+		return "Webhook received!"
+
+
+
+
+
+
+app.run(host='0.0.0.0', port=8000)
+dbx.files_upload(bytes(str(li)[1:-1], 'UTF-8'), '/Python/sample8.json')
+
+#with open("test3.json", "w") as f:
+	f.write(str(li))
+	f.close()
+
 
 
 

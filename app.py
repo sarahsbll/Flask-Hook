87ddbc8e3a1aa3a7b00ebc25e13876848f6a7787
sarahsbll <cynicoder@gmail.com>
2022-05-22 10:29:30 +0100

    debugging json file 6

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+	return "Hello World!"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        print("Data received from Webhook is: ", request.json)
-		with open("webhooked.json", "a") as outfile:
-        	json.dump(request.get_json(), outfile)
-        return "Webhook received!"
+	if request.method == 'POST':
+		print("Data received from Webhook is: ", request.json)
+		#with open("webhooked.json", "a") as outfile:
+			#json.dump(request.get_json(), outfile)
+		return "Webhook received!"

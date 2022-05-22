3b5afc8284763900ea9ae3f832ee38c2c684588d
sarahsbll <cynicoder@gmail.com>
2022-05-22 18:08:48 +0100

    debugging json file 26

--- app.py
+++ app.py
@@ -24,15 +24,12 @@ def webhook():
 		#print("Data received from Webhook is: ", request.json)
 		li.append(request.get_json())
 		#print(li)
+		dbx.files_upload(bytes(str(li), 'UTF-8'), '/Python/sample9.json')
 
 		return "Webhook received!"
 
 
 
-
-
-
-dbx.files_upload(bytes(str(li), 'UTF-8'), '/Python/sample9.json')
 
 #with open("test3.json", "w") as f:
 	#f.write(str(li))
